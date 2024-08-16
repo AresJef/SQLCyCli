067aee7525827b1a2aac3bba347bd651cca495c1
MacbookPro-14 <keppa1991@163.com>
2024-08-16 22:54:15 +0800

    Refactor code for improved organization and readability

--- src/sqlcycli/utils.pxd
+++ src/sqlcycli/utils.pxd
@@ -1,22 +1,14 @@
 # cython: language_level=3
 from libc.string cimport strchr
 from cpython.bytes cimport PyBytes_FromStringAndSize
-from cpython.bytes cimport PyBytes_GET_SIZE as bytes_len
-from cpython.unicode cimport PyUnicode_AsUTF8String
-from cpython.unicode cimport PyUnicode_GET_LENGTH as str_len
-from cpython.unicode cimport PyUnicode_ReadChar as read_char
-from cpython.unicode cimport PyUnicode_Substring as str_substr
-from cpython.unicode cimport PyUnicode_AsEncodedString as str_encode
-from sqlcycli.transcode cimport escape
-from sqlcycli._ssl cimport is_ssl, is_ssl_ctx
-from sqlcycli.charset cimport Charset, _charsets
-from sqlcycli import errors
+from cpython.unicode cimport PyUnicode_AsEncodedString
+from sqlcycli.charset cimport Charset
 
-# Utils: c-string 
+# Utils: string
 cdef inline object encode_str(object obj, char* encoding):
     """Encode string to bytes using 'encoding' with
     'surrogateescape' error handling `<'bytes'>`."""
-    return str_encode(obj, encoding, "surrogateescape")
+    return PyUnicode_AsEncodedString(obj, encoding, "surrogateescape")
 
 cdef inline bytes gen_length_encoded_integer(unsigned long long i):
     """Generate 'Length Coded Integer' for MySQL `<'bytes'>."""
@@ -270,210 +262,13 @@ cdef inline long long unpack_int64(char* data, unsigned long long pos):
     return <long long> unpack_uint64(data, pos)
 
 # Utils: Connection
-cdef inline bytes gen_connect_attrs(list attrs):
-    """Generate connection attributes for MySQL `<'bytes'>`."""
-    cdef bytearray arr = bytearray()
-    cdef bytes attr
-    for i in attrs:
-        attr = PyUnicode_AsUTF8String(i)
-        arr += gen_length_encoded_integer(bytes_len(attr))
-        arr += attr
-    return bytes(arr)
-
-# Utils: Connection validator
-cdef inline str validate_arg_str(object arg, str arg_name, str default):
-    """Validate if the string related argument is valid `<'str'>`."""
-    # Argument is None
-    if arg is None:
-        return default
-    # Argument is a string
-    if isinstance(arg, str):
-        return arg if str_len(arg) > 0 else default
-    # Invalid data type
-    raise errors.InvalidConnectionArgsError(
-        "Invalid '%s' argument: %r, expects <'str'> instead of %s."
-        % (arg_name, arg, type(arg))
-    )
-
-cdef inline object validate_arg_uint(object arg, str arg_name, unsigned int min_value, unsigned int max_value):
-    """Validate if the unsigned integer related argument is valid `<'unsigned int'>`."""
-    # Argument is None
-    if arg is None:
-        return None
-    # Argument is an integer
-    try:
-        value = int(arg)
-    except Exception as err:
-        raise errors.InvalidConnectionArgsError(
-            "Invalid '%s' argument: %r, expects <'int'> instead of %s."
-            % (arg_name, arg, type(arg))
-        ) from err
-    if not min_value <= value <= max_value:
-        raise errors.InvalidConnectionArgsError(
-            "Invalid '%s' argument: '%s', must be between %d and %d."
-            % (arg_name, value, min_value, max_value)
-        )
-    return value
-
-cdef inline bytes validate_arg_bytes(object arg, str arg_name, char* encoding, str default):
-    """Validate if the bytes related argument is valid `<'bytes'>`."""
-    # Argument is None
-    if arg is None:
-        if default is not None:
-            return str_encode(default, encoding, NULL)
-        return None
-    # Argument is a string
-    if isinstance(arg, str):
-        if str_len(arg) > 0:
-            return str_encode(arg, encoding, NULL)
-        if default is not None:
-            return str_encode(default, encoding, NULL)
-        return None
-    # Argument is bytes
-    if isinstance(arg, bytes):
-        if bytes_len(arg) > 0:
-            return arg
-        if default is not None:
-            return str_encode(default, encoding, NULL)
-        return None
-    # Invalid data type
-    raise errors.InvalidConnectionArgsError(
-        "Invalid '%s' argument: %r, expects <'str/bytes'> instead of %s."
-        % (arg_name, arg, type(arg))
-    )
-
-cdef inline Charset validate_charset(object charset, object collation, str default_charset):
-    """Validate if the 'charset' & 'collation' arguments are valid `<'Charset'>`."""
-    cdef:
-        str _charset = validate_arg_str(charset, "charset", default_charset)
-        str _collation = validate_arg_str(collation, "collation", None)
-    if _collation is None:
-        return _charsets.by_name(_charset)
-    else:
-        return _charsets.by_name_n_collation(_charset, _collation)
-
-cdef inline int validate_autocommit(object autocommit) except -2:
-    """Validate if the 'autocommit' argument is valid `<'int'>`.
-    Returns -1 if use server default, or 1/0 as autocommit value.
-    """
-    if autocommit is None:
-        return -1
-    return 1 if bool(autocommit) else 0
-
-cdef inline unsigned int validate_max_allowed_packet(object max_allowed_packet, int default, int maximum):
-    """Validate if the 'max_allowed_packet' argument is valid `<'int'>`."""
-    # Argument is None
-    if max_allowed_packet is None:
-        return default
-    # Argument is an integer
-    cdef long long value
-    if isinstance(max_allowed_packet, int):
-        try:
-            value = max_allowed_packet
-        except Exception as err:
-            raise errors.InvalidConnectionArgsError(
-                "Invalid 'max_allowed_packet' argument: %s, "
-                "expects <'str/int'> instead of %s."
-                % (max_allowed_packet, type(max_allowed_packet))
-            ) from err
-        # Validate value
-        if not 0 < value <= maximum:
-            raise errors.InvalidConnectionArgsError(
-                "Invalid 'max_allowed_packet' argument: "
-                "'%s', must be between 1 and %s."
-                % (value, maximum)
-            )
-        return value
-    # Argument is not a string
-    if not isinstance(max_allowed_packet, str):
-        raise errors.InvalidConnectionArgsError(
-            "Invalid 'max_allowed_packet' argument: %r, "
-            "expects <'str/int'> instead of %s."
-            % (max_allowed_packet, type(max_allowed_packet))
-        )
-    # Parse value from string
-    cdef Py_ssize_t length = str_len(max_allowed_packet)
-    cdef unsigned int mult
-    cdef Py_UCS4 ch
-    ch = read_char(max_allowed_packet, length - 1)
-    # . skip M/K/G[B] suffix
-    if ch in ("B", "b"):
-        length -= 1
-        ch = read_char(max_allowed_packet, length - 1)
-    # . K/KiB suffix
-    if ch in ("K", "k"):
-        length -= 1
-        mult = 1_024
-    # . M/MiB suffix
-    elif ch in ("M", "m"):
-        length -= 1
-        mult = 1_048_576
-    # . G/GiB suffix
-    elif ch in ("G", "g"):
-        length -= 1
-        mult = 1_073_741_824
-    # . no suffix
-    else:
-        mult = 1
-    # . parse to integer
-    if length < 1:
-        raise errors.InvalidConnectionArgsError(
-            "Invalid 'max_allowed_packet' argument: '%s' %s."
-            % (max_allowed_packet, type(max_allowed_packet))
-        )
-    # Parse integer
-    try:
-        value = int(str_substr(max_allowed_packet, 0, length))
-    except Exception as err:
-        raise errors.InvalidConnectionArgsError(
-            "Invalid 'max_allowed_packet' argument: '%s' %s."
-            % (max_allowed_packet, type(max_allowed_packet))
-        ) from err
-    value *= mult
-    # Validate value
-    if not 0 < value <= maximum:
-        raise errors.InvalidConnectionArgsError(
-            "Invalid 'max_allowed_packet' argument: "
-            "'%s' (%s), must be between 1 and %s."
-            % (max_allowed_packet, value, maximum)
-        )
-    return value
-
-cdef inline str validate_sql_mode(object sql_mode, char* encoding):
-    """Validate if the 'sql_mode' argument is valid `<'str'>`."""
-    # Argument is None
-    if sql_mode is None:
-        return None
-    # Argument is a string
-    if isinstance(sql_mode, str):
-        if str_len(sql_mode) > 0:
-            return escape(sql_mode, encoding, False, False)
-        return None
-    # Invalid data type
-    raise errors.InvalidConnectionArgsError(
-        "Invalid 'sql_mode' argument: %r, expects <'str'> instead of %s."
-        % (sql_mode, type(sql_mode))
-    )
-
-cdef inline object validate_cursor(object cursor, object cursor_class):
-    """Validate if the 'cursor' argument is valid `<'type[Cursor]'>`."""
-    if cursor is None:
-        return cursor_class
-    if type(cursor) is not type or not issubclass(cursor, cursor_class):
-        raise errors.InvalidConnectionArgsError(
-            "Invalid 'cursor' %r, must be type of %r." % (cursor, cursor_class)
-        )
-    return cursor
-
-cdef inline object validate_ssl(object ssl):
-    """Validate if the 'ssl' argument is valid `<'ssl.SSLContext'>`."""
-    if ssl is None:
-        return None
-    if is_ssl(ssl):
-        return ssl.context if ssl else None
-    if is_ssl_ctx(ssl):
-        return ssl
-    raise errors.InvalidConnectionArgsError(
-        "Invalid 'ssl' argument: %r, expects <'SSL/SSLContext'> "
-        "instead of %s." % (ssl, type(ssl))
-    )
+cdef bytes gen_connect_attrs(list attrs)
+cdef str validate_arg_str(object arg, str arg_name, str default)
+cdef object validate_arg_uint(object arg, str arg_name, unsigned int min_val, unsigned int max_val)
+cdef bytes validate_arg_bytes(object arg, str arg_name, char* encoding, str default)
+cdef Charset validate_charset(object charset, object collation, str default_charset)
+cdef int validate_autocommit(object auto) except -2
+cdef int validate_max_allowed_packet(object max_allowed_packet, int default, int maximum)
+cdef str validate_sql_mode(object sql_mode, char* encoding)
+cdef object validate_cursor(object cursor, object cursor_class)
+cdef object validate_ssl(object ssl)
--- src/sqlcycli/utils.py
+++ src/sqlcycli/utils.py
@@ -2,24 +2,273 @@
 
 # Cython imports
 import cython
+from cython.cimports.cpython.bytes import PyBytes_GET_SIZE as bytes_len  # type: ignore
+from cython.cimports.cpython.unicode import PyUnicode_GET_LENGTH as str_len  # type: ignore
+from cython.cimports.cpython.unicode import PyUnicode_ReadChar as read_char  # type: ignore
+from cython.cimports.cpython.unicode import PyUnicode_Substring as str_substr  # type: ignore
+from cython.cimports.cpython.unicode import PyUnicode_AsUTF8String as encode_str_utf8  # type: ignore
+from cython.cimports.sqlcycli.transcode import escape  # type: ignore
+from cython.cimports.sqlcycli._ssl import is_ssl, is_ssl_ctx  # type: ignore
+from cython.cimports.sqlcycli.charset import Charset, _charsets  # type: ignore
+
+# Python imports
+from sqlcycli.charset import Charset
+from sqlcycli.transcode import escape
+from sqlcycli._ssl import is_ssl, is_ssl_ctx
+from sqlcycli import errors
 
 
-### All utils functions are in the utils.pxd file ###
+# Utils: Connection ---------------------------------------------------------------------------
+@cython.cfunc
+@cython.inline(True)
+def gen_connect_attrs(attrs: list[str]) -> bytes:
+    """(cfunc) Generate connect attributes for Connection `<'bytes'>`."""
+    arr: list = []
+    for i in attrs:
+        attr = encode_str_utf8(i)
+        arr.append(gen_length_encoded_integer(bytes_len(attr)))  # type: ignore
+        arr.append(attr)
+    return b"".join(arr)
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_arg_str(arg: object, arg_name: str, default: str) -> str:
+    """(cfunc) Validate if a string argument is valid `<'str'>`."""
+    if arg is None:
+        return default
+    if isinstance(arg, str):
+        return arg if str_len(arg) > 0 else default
+    raise errors.InvalidConnectionArgsError(
+        "Invalid '%s' argument: %r.\n"
+        "Expects <'str'> instead of %s." % (arg_name, arg, type(arg))
+    )
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_arg_uint(
+    arg: object,
+    arg_name: str,
+    min_val: cython.uint,
+    max_val: cython.uint,
+) -> object:
+    """(cfunc) Validate if an unsigned integer argument is valid `<'unsigned int'>`."""
+    if arg is None:
+        return None
+    try:
+        val: cython.longlong = int(arg)
+    except Exception as err:
+        raise errors.InvalidConnectionArgsError(
+            "Invalid '%s' argument: %r.\n"
+            "Expects <'int'> instead of %s." % (arg_name, arg, type(arg))
+        ) from err
+    if not min_val <= val <= max_val:
+        raise errors.InvalidConnectionArgsError(
+            "Invalid '%s' argument: %r.\n"
+            "Expects an integer between %d and %d." % (arg_name, val, min_val, max_val)
+        )
+    return val
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_arg_bytes(
+    arg: object,
+    arg_name: str,
+    encoding: cython.pchar,
+    default: str,
+) -> bytes:
+    """(cfunc) Validate if a bytes argument is valid `<'bytes'>`."""
+    if arg is None:
+        if default is not None:
+            return encode_str(default, encoding)  # type: ignore
+        return None
+    if isinstance(arg, str):
+        if str_len(arg) > 0:
+            return encode_str(arg, encoding)  # type: ignore
+        if default is not None:
+            return encode_str(default, encoding)  # type: ignore
+        return None
+    if isinstance(arg, bytes):
+        if bytes_len(arg) > 0:
+            return arg
+        if default is not None:
+            return encode_str(default, encoding)  # type: ignore
+        return None
+    raise errors.InvalidConnectionArgsError(
+        "Invalid '%s' argument: %r.\n"
+        "Expects <'str/bytes'> instead of %s." % (arg_name, arg, type(arg))
+    )
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_charset(
+    charset: object,
+    collation: object,
+    default_charset: str,
+) -> Charset:
+    """(cfunc) Validate if 'charset' & 'collation' arguments are valid `<'Charset'>`."""
+    ch: str = validate_arg_str(charset, "charset", default_charset)
+    cl: str = validate_arg_str(collation, "collation", None)
+    if cl is None:
+        return _charsets.by_name(ch)
+    else:
+        return _charsets.by_name_n_collation(ch, cl)
+
+
+@cython.cfunc
+@cython.inline(True)
+@cython.exceptval(-2, check=False)
+def validate_autocommit(auto: object) -> cython.int:
+    """(cfunc) Validate if the 'autocommit' argument is valid `<'int'>`.
+
+    Returns -1 if use server default, else 0/1
+    as disable/enable autocommit mode.
+    """
+    if auto is None:
+        return -1
+    else:
+        return 1 if bool(auto) else 0
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_max_allowed_packet(
+    max_allowed_packet: object,
+    default: cython.int,
+    max_val: cython.int,
+) -> cython.int:
+    """(cfunc) Validate if the 'max_allowed_packet' argument is valid `<'int'>`."""
+    # Argument is None
+    if max_allowed_packet is None:
+        return default
+    # Argument is integer
+    if isinstance(max_allowed_packet, int):
+        try:
+            value: cython.longlong = max_allowed_packet
+        except Exception as err:
+            raise errors.InvalidConnectionArgsError(
+                "Invalid 'max_allowed_packet' argument: %r.\n"
+                "Expects <'str/int'> instead of %s."
+                % (max_allowed_packet, type(max_allowed_packet))
+            ) from err
+    # Arugment is string
+    elif isinstance(max_allowed_packet, str):
+        length: cython.Py_ssize_t = str_len(max_allowed_packet)
+        ch: cython.Py_UCS4 = read_char(max_allowed_packet, length - 1)
+        # . skip M/K/G[B] suffix
+        if ch in ("B", "b"):
+            length -= 1
+            ch = read_char(max_allowed_packet, length - 1)
+        # . K/KiB suffix
+        if ch in ("K", "k"):
+            length -= 1
+            mult: cython.int = 1_024
+        # . M/MiB suffix
+        elif ch in ("M", "m"):
+            length -= 1
+            mult: cython.int = 1_048_576
+        # . G/GiB suffix
+        elif ch in ("G", "g"):
+            length -= 1
+            mult: cython.int = 1_073_741_824
+        # . no unit suffix
+        else:
+            mult: cython.int = 1
+        # . parse integer
+        try:
+            if length < 1:
+                raise ValueError("Not enough charactors.")
+            value: cython.longlong = int(str_substr(max_allowed_packet, 0, length))
+        except Exception as err:
+            raise errors.InvalidConnectionArgsError(
+                "Invalid 'max_allowed_packet' argument: %r %s."
+                % (max_allowed_packet, type(max_allowed_packet))
+            ) from err
+        value *= mult
+    # Invalid
+    else:
+        raise errors.InvalidConnectionArgsError(
+            "Invalid 'max_allowed_packet' argument: %r.\n"
+            "Expects <'str/int'> instead of %s."
+            % (max_allowed_packet, type(max_allowed_packet))
+        )
+    # Validate value
+    if not 0 < value <= max_val:
+        raise errors.InvalidConnectionArgsError(
+            "Invalid 'max_allowed_packet' argument: %r.\n"
+            "Value in bits must between 1 and %s." % (max_allowed_packet, max_val)
+        )
+    return value
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_sql_mode(sql_mode: object, encoding: cython.pchar) -> str:
+    """(cfunc) Validate if the 'sql_mode' argument is valid `<'str'>`."""
+    if sql_mode is None:
+        return None
+    if isinstance(sql_mode, str):
+        if str_len(sql_mode) > 0:
+            return escape(sql_mode, encoding, False, False)
+        return None
+    raise errors.InvalidConnectionArgsError(
+        "Invalid 'sql_mode' argument: %r.\n"
+        "Expects <'str'> instead of %s." % (sql_mode, type(sql_mode))
+    )
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_cursor(cursor: object, cursor_class: object) -> object:
+    """(cfunc) Validate if the 'cursor' argument is valid `<'object'>`."""
+    if cursor is None:
+        return cursor_class
+    try:
+        if not issubclass(cursor, cursor_class):
+            raise TypeError("Invalid cursor class.")
+    except Exception as err:
+        raise errors.InvalidConnectionArgsError(
+            "Invalid 'cursor' argument: %r.\n"
+            "Expects type [class] of %r." % (cursor, cursor_class)
+        ) from err
+    return cursor
+
+
+@cython.cfunc
+@cython.inline(True)
+def validate_ssl(ssl: object) -> object:
+    """(cfunc) Validate if the 'ssl' argument is valid `<'ssl.SSLContext'>`."""
+    if ssl is None:
+        return None
+    if is_ssl(ssl):
+        return ssl.context if ssl else None
+    if is_ssl_ctx(ssl):
+        return ssl
+    raise errors.InvalidConnectionArgsError(
+        "Invalid 'ssl' argument: %r.\n"
+        "Expects <'SSL/SSLContext'> instead of %s." % (ssl, type(ssl))
+    )
+
+
+### The REST util functions are in the utils.pxd file ###
 ### The following functions are for testing purposes only ###
-def test_all(fast: cython.bint = True) -> None:
-    _test_uint8(fast)
-    _test_int8(fast)
-    _test_uint16(fast)
-    _test_int16(fast)
-    _test_uint24(fast)
-    _test_int24(fast)
-    _test_uint32(fast)
-    _test_int32(fast)
-    _test_uint64(fast)
-    _test_int64(fast)
+def test_all() -> None:
+    _test_pack_unpack_int8()
+    _test_pack_unpack_uint8()
+    _test_pack_unpack_int16()
+    _test_pack_unpack_uint16()
+    _test_pack_unpack_int24()
+    _test_pack_unpack_uint24()
+    _test_pack_unpack_int32()
+    _test_pack_unpack_uint32()
+    _test_pack_unpack_int64()
+    _test_pack_unpack_uint64()
 
 
-def _test_uint8(fast: cython.bint) -> None:
+def _test_pack_unpack_uint8() -> None:
     print("Test Pack/Unpack uint8")
     for val in range(0, 256):
         b = pack_uint8(val)  # type: ignore
@@ -28,7 +277,7 @@ def _test_uint8(fast: cython.bint) -> None:
         print("- pass: %s" % val, end="\r")
 
 
-def _test_int8(fast: cython.bint) -> None:
+def _test_pack_unpack_int8() -> None:
     print("Test Pack/Unpack int8")
     for val in range(-128, 128):
         b = pack_int8(val)  # type: ignore
@@ -37,7 +286,7 @@ def _test_int8(fast: cython.bint) -> None:
         print("- pass: %s" % val, end="\r")
 
 
-def _test_uint16(fast: cython.bint) -> None:
+def _test_pack_unpack_uint16() -> None:
     print("Test Pack/Unpack uint16")
     for val in range(0, 65536):
         b = pack_uint16(val)  # type: ignore
@@ -46,7 +295,7 @@ def _test_uint16(fast: cython.bint) -> None:
         print("- pass: %s" % val, end="\r")
 
 
-def _test_int16(fast: cython.bint) -> None:
+def _test_pack_unpack_int16() -> None:
     print("Test Pack/Unpack int16")
     for val in range(-32768, 32768):
         b = pack_int16(val)  # type: ignore
@@ -55,103 +304,67 @@ def _test_int16(fast: cython.bint) -> None:
         print("- pass: %s" % val, end="\r")
 
 
-def _test_uint24(fast: cython.bint) -> None:
-    print("Test Pack/Unpack uint24")
-    if fast:
-        for val in (0, 1, 16777214, 16777215):
-            b = pack_uint24(val)  # type: ignore
-            i = unpack_uint24(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-    else:
-        for val in range(0, 16777216):
-            b = pack_uint24(val)  # type: ignore
-            i = unpack_uint24(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-
-
-def _test_int24(fast: cython.bint) -> None:
+def _test_pack_unpack_int24() -> None:
     print("Test Pack/Unpack int24")
-    if fast:
-        for val in (-8388608, -8388607, 0, 8388606, 8388607):
-            b = pack_int24(val)  # type: ignore
-            i = unpack_int24(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-    else:
-        for val in range(-8388608, 8388608):
-            b = pack_int24(val)  # type: ignore
-            i = unpack_int24(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
+    for val in (-8388608, -8388607, 0, 8388606, 8388607):
+        b = pack_int24(val)  # type: ignore
+        i = unpack_int24(b, 0)  # type: ignore
+        assert val == i, f"val {val} vs i {i}"
+        print("- pass: %s" % val, end="\r")
 
 
-def _test_uint32(fast: cython.bint) -> None:
-    print("Test Pack/Unpack uint32")
-    if fast:
-        for val in (0, 1, 4294967294, 4294967295):
-            b = pack_uint32(val)  # type: ignore
-            i = unpack_uint32(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-    else:
-        for val in range(0, 4294967296):
-            b = pack_uint32(val)  # type: ignore
-            i = unpack_uint32(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
+def _test_pack_unpack_uint24() -> None:
+    print("Test Pack/Unpack uint24")
+    for val in (0, 1, 16777213, 16777214, 16777215):
+        b = pack_uint24(val)  # type: ignore
+        i = unpack_uint24(b, 0)  # type: ignore
+        assert val == i, f"val {val} vs i {i}"
+        print("- pass: %s" % val, end="\r")
 
 
-def _test_int32(fast: cython.bint) -> None:
+def _test_pack_unpack_int32() -> None:
     print("Test Pack/Unpack int32")
-    if fast:
-        for val in (-2147483648, -2147483647, 0, 2147483646, 2147483647):
-            b = pack_int32(val)  # type: ignore
-            i = unpack_int32(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-    else:
-        for val in range(-2147483648, 2147483648):
-            b = pack_int32(val)  # type: ignore
-            i = unpack_int32(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
+    for val in (-2147483648, -2147483647, 0, 2147483646, 2147483647):
+        b = pack_int32(val)  # type: ignore
+        i = unpack_int32(b, 0)  # type: ignore
+        assert val == i, f"val {val} vs i {i}"
+        print("- pass: %s" % val, end="\r")
 
 
-def _test_uint64(fast: cython.bint) -> None:
-    print("Test Pack/Unpack uint64")
-    if fast:
-        for val in (0, 1, 18446744073709551614, 18446744073709551615):
-            b = pack_uint64(val)  # type: ignore
-            i = unpack_uint64(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-    else:
-        for val in range(0, 18446744073709551616):
-            b = pack_uint64(val)  # type: ignore
-            i = unpack_uint64(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
+def _test_pack_unpack_uint32() -> None:
+    print("Test Pack/Unpack uint32")
+    for val in (0, 1, 4294967293, 4294967294, 4294967295):
+        b = pack_uint32(val)  # type: ignore
+        i = unpack_uint32(b, 0)  # type: ignore
+        assert val == i, f"val {val} vs i {i}"
+        print("- pass: %s" % val, end="\r")
 
 
-def _test_int64(fast: cython.bint) -> None:
+def _test_pack_unpack_int64() -> None:
     print("Test Pack/Unpack int64")
-    if fast:
-        for val in (
-            -9223372036854775808,
-            -9223372036854775807,
-            0,
-            9223372036854775806,
-            9223372036854775807,
-        ):
-            b = pack_int64(val)  # type: ignore
-            i = unpack_int64(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
-    else:
-        for val in range(-9223372036854775808, 9223372036854775808):
-            b = pack_int64(val)  # type: ignore
-            i = unpack_int64(b, 0)  # type: ignore
-            assert val == i, f"val {val} vs i {i}"
-            print("- pass: %s" % val, end="\r")
+    for val in (
+        -9223372036854775808,
+        -9223372036854775807,
+        0,
+        9223372036854775806,
+        9223372036854775807,
+    ):
+        b = pack_int64(val)  # type: ignore
+        i = unpack_int64(b, 0)  # type: ignore
+        assert val == i, f"val {val} vs i {i}"
+        print("- pass: %s" % val, end="\r")
+
+
+def _test_pack_unpack_uint64() -> None:
+    print("Test Pack/Unpack uint64")
+    for val in (
+        0,
+        1,
+        18446744073709551613,
+        18446744073709551614,
+        18446744073709551615,
+    ):
+        b = pack_uint64(val)  # type: ignore
+        i = unpack_uint64(b, 0)  # type: ignore
+        assert val == i, f"val {val} vs i {i}"
+        print("- pass: %s" % val, end="\r")
